b94681a1d28bb647e702443ee6f6b73cf269cbd2
Michael Vierling <mvierling@gmail.com>
2023-07-19 14:04:43 -0700

    add build step

--- IOTPacketIngest/run.ps1
+++ IOTPacketIngest/run.ps1
@@ -2,4 +2,5 @@ $Env:AWS_ACCESS_KEY="XXXXX"
 $Env:AWS_SECRET_ACCESS_KEY="SSSSS"
 $Env:AWS_SESSION_TOKEN="TTTTT"
 
+docker build -t iot-ingest-image -f Dockerfile .
 docker run -it -e AWS_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN --rm iot-ingest-image
